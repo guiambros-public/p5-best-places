7c10e159b2778c224fcd2a1c5d5dd9886d616271
Gui Ambros <gui@wrgms.com>
2015-06-06 22:11:28 -0400

    Click event added to sidebar, plus colored bounding boxes
    Click event now changes the colors of sidebar buttons, and populates
    the description box, as well as the image.

--- src/js/views.js
+++ src/js/views.js
@@ -9,9 +9,14 @@ var app = app || {};
 
         el: "#sidebar-container",
 
+        events: {
+            'click a': 'clicked',
+        },
+
         initialize: function () {
             this.listenTo(app.places, "change", this.render);
             this.render();
+
     	},
 
     	render: function () {
@@ -20,6 +25,20 @@ var app = app || {};
             var template = _.template( $("#sidebar-item-template").html() );
             this.$el.append( template( ) );
             return this;
+        },
+
+        clicked: function(e) {
+            app.e = e;
+            e.preventDefault();
+            var id = $(e.currentTarget).data("id");
+            var item = app.places.get(id);
+            var name = item.get("name");
+            $("a").toggleClass("active", false);
+            $(e.currentTarget).toggleClass("active", true);
+            $("#image-box").attr("src", item.get("image"));
+            $("#description-text").html(item.get("description"));
+            console.log("Click detected - id:[ " + id + "], name: [" + name + "]");
+
         },
 
     });
